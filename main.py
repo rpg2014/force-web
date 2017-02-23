b1b33425d49f8667609e57124e7124c04c5f9e38
BuildTools <unconfigured@null.spigotmc.org>
2017-02-23 08:42:19 -0500

    Created person class, and tested it

---
+++ main.py
@@ -0,0 +1,10 @@
+from person import Person
+
+a="hi"
+b="by"
+c="cat"
+
+per = Person("Parker", [a,b,c])
+
+print per.name
+print per.connections
---
+++ person.py
@@ -0,0 +1,16 @@
+class Person:
+    
+
+    def __init__(self,name,connections):
+        self.name = name
+        self.connections = connections
+
+
+
+    def getName(self):
+        return self.name
+
+    def getConnections(self):
+        return self.connections
+
+            
